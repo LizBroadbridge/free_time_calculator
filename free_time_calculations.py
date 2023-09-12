c0179a8c2504ef43753c5f17160e71272b8f343e
thisdotrob <this.rob@protonmail.com>
2023-09-12 09:43:39 +0100

    Fix use of deprecated method

--- free_time_calculations.py
+++ free_time_calculations.py
@@ -22,6 +22,6 @@ def free_time_calc():
 
     result_df["Total"] = result_df.iloc[:, 1:].sum(axis=1)
 
-    result_df.iloc[:,1:] = result_df.iloc[:,1:].applymap(lambda x: f"{int(x // 60)} hours {int(x % 60)} minutes")
+    result_df.iloc[:,1:] = result_df.iloc[:,1:].map(lambda x: f"{int(x // 60)} hours {int(x % 60)} minutes")
 
     return result_df
