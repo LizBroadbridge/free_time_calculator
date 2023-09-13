cb4446a78a3d940985b31f9605f98fa30a99539a
thisdotrob <this.rob@protonmail.com>
2023-09-13 11:08:50 +0100

    Change to two week long schedule

--- free_time_calculations.py
+++ free_time_calculations.py
@@ -16,7 +16,7 @@ def free_time_calc(csv_string):
     result_df = free_time_df.groupby(["Day", "Text"])["Duration"].sum().reset_index()
     result_df = result_df.pivot(index="Day", columns="Text", values="Duration").reset_index().fillna(0)
 
-    ordered_days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    ordered_days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Monday2", "Tuesday2", "Wednesday2", "Thursday2", "Friday2", "Saturday2", "Sunday2"]
     cat_type = CategoricalDtype(categories=ordered_days_of_week, ordered=True)
     result_df["Day"] = result_df["Day"].astype(cat_type)
     result_df = result_df.sort_values(by="Day")
