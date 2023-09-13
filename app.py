ad87b6553215ed1c4ed92b21b9356e9c4315488a
thisdotrob <this.rob@protonmail.com>
2023-09-13 13:15:19 +0100

    Hacky styling

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ external_stylesheets = [
         "href": "https://fonts.googleapis.com/css?family=Rubik",
         "rel": "stylesheet",
     },
+    dbc.themes.BOOTSTRAP,
 ]
 
 app = Dash(__name__, external_stylesheets=external_stylesheets)
@@ -27,145 +28,167 @@ app.layout = html.Div(
                     ),
                     className="header-description",
                 ),
-                html.Div(id='output-filename'),
-                dcc.Upload(
-                    id='upload-data',
-                    children=html.Div([
-                        'Schedule A: Drag and Drop or ',
-                        html.A('Select a File')
-                    ]),
-                    style={
-                        'width': '100%',
-                        'height': '60px',
-                        'lineHeight': '60px',
-                        'borderWidth': '1px',
-                        'borderStyle': 'dashed',
-                        'borderRadius': '5px',
-                        'textAlign': 'center',
-                        'margin': '10px'
-                    },
-                    multiple=False
-                ),
-                html.Div(id='output-filename-b'),
-                dcc.Upload(
-                    id='upload-data-b',
-                    children=html.Div([
-                        'Schedule B: Drag and Drop or ',
-                        html.A('Select a File')
-                    ]),
-                    style={
-                        'width': '100%',
-                        'height': '60px',
-                        'lineHeight': '60px',
-                        'borderWidth': '1px',
-                        'borderStyle': 'dashed',
-                        'borderRadius': '5px',
-                        'textAlign': 'center',
-                        'margin': '10px'
-                    },
-                    multiple=False
-                ),
-                dbc.Button(
-                    "Compare",
-                    id="compare-button",
-                ),
-                dcc.Dropdown(
-                    id='dropdown-toggle',
-                    options=[
-                        {'label': 'Show DataTable', 'value': 'show'},
-                        {'label': 'Hide DataTable', 'value': 'hide'}
-                    ],
-                    value='hide'
-                ),
-                html.P(
-                    children=(
-                        "Schedule A:"
-                    ),
-                ),
-                dcc.Loading(
-                    id="loading-table",
-                    children=[
-                        html.Div(
+                html.Div(
+                    [
+                        dbc.Row(
                             [
-                                dash_table.DataTable(
-                                    id="data-table",
-                                    columns=[
-                                        {"name": col, "id": col}
-                                        for col in [
-                                            "Day",
-                                            "Free",
-                                            "Free (at Gymnastics)",
-                                            "Free (boys at home)",
-                                            "Free (boys in bed)",
-                                            "Free (with Milo at home)",
-                                            "Total",
-                                        ]
-                                    ],
+                                html.Div(id='output-filename', className="mx_auto"),
+                                dcc.Upload(
+                                    id='upload-data',
+                                    children=html.Div([
+                                        'Schedule A: Drag and Drop or ',
+                                        html.A('Select a File')
+                                    ]),
+                                    className="mx-auto",
+                                    style={
+                                        'width': '500px',
+                                        'height': '60px',
+                                        'lineHeight': '60px',
+                                        'borderWidth': '1px',
+                                        'borderStyle': 'dashed',
+                                        'borderRadius': '5px',
+                                        'textAlign': 'center',
+                                        'margin': '10px'
+                                    },
+                                    multiple=False
+                                ),
+                            ]
+                        ),
+                        dbc.Row(
+                            [
+                                html.Div(id='output-filename-b', className="mx_auto"),
+                                dcc.Upload(
+                                    id='upload-data-b',
+                                    children=html.Div([
+                                        'Schedule B: Drag and Drop or ',
+                                        html.A('Select a File')
+                                    ]),
+                                    className="mx-auto",
+                                    style={
+                                        'width': '500px',
+                                        'height': '60px',
+                                        'lineHeight': '60px',
+                                        'borderWidth': '1px',
+                                        'borderStyle': 'dashed',
+                                        'borderRadius': '5px',
+                                        'textAlign': 'center',
+                                        'margin': '10px'
+                                    },
+                                    multiple=False
                                 ),
                             ]
                         ),
                     ],
-                    type="default",
                 ),
-                html.P(
-                    children=(
-                        "Schedule B:"
-                    ),
-                ),
-                dcc.Loading(
-                    id="loading-table-b",
-                    children=[
-                        html.Div(
-                            [
-                                dash_table.DataTable(
-                                    id="data-table-b",
-                                    columns=[
-                                        {"name": col, "id": col}
-                                        for col in [
-                                            "Day",
-                                            "Free",
-                                            "Free (at Gymnastics)",
-                                            "Free (boys at home)",
-                                            "Free (boys in bed)",
-                                            "Free (with Milo at home)",
-                                            "Total",
-                                        ]
-                                    ],
+                dbc.Container(
+                    dbc.Stack(
+                        [
+                            dbc.Row([
+                                dbc.Button(
+                                    "Compare",
+                                    id="compare-button",
+                                    className="mx-auto",
+                                    color="secondary",
+                                    style={
+                                        "width": "100px"
+                                    },
                                 ),
-                            ]
-                        ),
-                    ],
-                    type="default",
-                ),
-                html.P(
-                    children=(
-                        "Delta:"
-                    ),
-                ),
-                dcc.Loading(
-                    id="loading-delta-table",
-                    children=[
-                        html.Div(
-                            [
-                                dash_table.DataTable(
-                                    id="delta-table",
-                                    columns=[
-                                        {"name": col, "id": col}
-                                        for col in [
-                                            "Day",
-                                            "Free",
-                                            "Free (at Gymnastics)",
-                                            "Free (boys at home)",
-                                            "Free (boys in bed)",
-                                            "Free (with Milo at home)",
-                                            "Total",
-                                        ]
+                            ]),
+                            dbc.Row([
+                                dcc.Dropdown(
+                                    id='dropdown-toggle',
+                                    options=[
+                                        {'label': 'Show DataTable', 'value': 'show'},
+                                        {'label': 'Hide DataTable', 'value': 'hide'}
                                     ],
+                                    value='hide',
+                                    style={
+                                        "width": "200px"
+                                    },
                                 ),
-                            ]
-                        ),
-                    ],
-                    type="default",
+                            ]),
+                            "Schedule A:",
+                            dcc.Loading(
+                                id="loading-table",
+                                children=[
+                                    html.Div(
+                                        [
+                                            dash_table.DataTable(
+                                                id="data-table",
+                                                columns=[
+                                                    {"name": col, "id": col}
+                                                    for col in [
+                                                        "Day",
+                                                        "Free",
+                                                        "Free (at Gymnastics)",
+                                                        "Free (boys at home)",
+                                                        "Free (boys in bed)",
+                                                        "Free (with Milo at home)",
+                                                        "Total",
+                                                    ]
+                                                ],
+                                            ),
+                                        ]
+                                    ),
+                                ],
+                                type="default",
+                            ),
+                            "Schedule B:",
+                            dcc.Loading(
+                                id="loading-table-b",
+                                children=[
+                                    html.Div(
+                                        [
+                                            dash_table.DataTable(
+                                                id="data-table-b",
+                                                columns=[
+                                                    {"name": col, "id": col}
+                                                    for col in [
+                                                        "Day",
+                                                        "Free",
+                                                        "Free (at Gymnastics)",
+                                                        "Free (boys at home)",
+                                                        "Free (boys in bed)",
+                                                        "Free (with Milo at home)",
+                                                        "Total",
+                                                    ]
+                                                ],
+                                            ),
+                                        ]
+                                    ),
+                                ],
+                                type="default",
+                            ),
+                            "Delta:",
+                            dcc.Loading(
+                                id="loading-delta-table",
+                                children=[
+                                    html.Div(
+                                        [
+                                            dash_table.DataTable(
+                                                id="delta-table",
+                                                columns=[
+                                                    {"name": col, "id": col}
+                                                    for col in [
+                                                        "Day",
+                                                        "Free",
+                                                        "Free (at Gymnastics)",
+                                                        "Free (boys at home)",
+                                                        "Free (boys in bed)",
+                                                        "Free (with Milo at home)",
+                                                        "Total",
+                                                    ]
+                                                ],
+                                            ),
+                                        ]
+                                    ),
+                                ],
+                                type="default",
+                            ),
+                        ],
+                        gap=3,
+                    ),
+                    fluid=True,
                 ),
             ],
             className="header",
@@ -177,10 +200,11 @@ prev_contents = None
 
 def filename_children(filename):
     if filename is not None:
-        return html.P(
+        return html.Div(
             children=filename,
+            className="mx-auto",
             style={
-                'width': '100%',
+                'width': '500px',
                 'height': '60px',
                 'lineHeight': '60px',
                 'borderWidth': '1px',
